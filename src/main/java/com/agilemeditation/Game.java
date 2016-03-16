6522e15d8fb3f9de30c87a7f3b9cfec9a0a8657a
Augusto Rodriguez <augusto.rodriguez@gmail.com>
2016-03-16 20:59:29 +0000

    tidied up code a bit. Still think that the logic is not as clear as I would like it to be

--- src/main/java/com/agilemeditation/Game.java
+++ src/main/java/com/agilemeditation/Game.java
@@ -1,6 +1,8 @@
 package com.agilemeditation;
 
 public class Game {
+  private static final int MAX_PINS = 10;
+
   private int score;
   private Roll previousScores[] = new Roll[2];
   private int rollCount;
@@ -10,41 +12,46 @@ public class Game {
     return score;
   }
 
-  public void roll(int rollScore) {
+  public void roll(int currentRollScore) {
     //add bonuses
-    if (getRoll(rollCount - 2).shouldAddSecondNextRollAsBonus()) {
-      score += rollScore;
+    if (getRoll(rollCount - 2).isStrike()) {
+      score += currentRollScore;
     }
-    if (getRoll(rollCount - 1).shouldAddNextRollAsBonus()) {
-      score += rollScore;
+    if (getRoll(rollCount - 1).isSpareOrStrike()) {
+      score += currentRollScore;
     }
 
     if (nextRollTerminatesFrame) {
       //Spare or 'normal' frame scenarios
-      if (getRoll(rollCount - 1).getScore() + rollScore == 10) {
-        previousScores[rollCount % 2] = new Roll(rollScore, true, false);
-        nextRollTerminatesFrame = false;
+      if (getRoll(rollCount - 1).getScore() + currentRollScore == MAX_PINS) {
+        storeCurrentRoll(new Roll(currentRollScore, Bonus.SPARE));
       } else {
-        previousScores[rollCount % 2] = new Roll(rollScore, false, false);
-        nextRollTerminatesFrame = false;
+        storeCurrentRoll(new Roll(currentRollScore, Bonus.NONE));
       }
-    } else if (rollScore == 10) {
-      // Strike scenario
-      previousScores[rollCount % 2] = new Roll(rollScore, true, true);
       nextRollTerminatesFrame = false;
     } else {
-      // First roll 'normal' scenario
-      previousScores[rollCount % 2] = new Roll(rollScore, false, false);
-      nextRollTerminatesFrame = true;
+      if (currentRollScore == MAX_PINS) {
+        // Strike scenario
+        storeCurrentRoll(new Roll(currentRollScore, Bonus.STRIKE));
+        nextRollTerminatesFrame = false;
+      } else {
+        // First roll 'normal' scenario
+        storeCurrentRoll(new Roll(currentRollScore, Bonus.NONE));
+        nextRollTerminatesFrame = true;
+      }
     }
 
-    score += rollScore;
+    score += currentRollScore;
     rollCount++;
+  }
+
+  private void storeCurrentRoll(Roll roll) {
+    previousScores[rollCount % 2] = roll;
   }
 
   private Roll getRoll(int rollNumber) {
     if (rollNumber < 0) {
-      return new Roll(0, false, false);
+      return new Roll(0, Bonus.NONE);
     }
     return previousScores[rollNumber % 2];
   }
@@ -52,25 +59,29 @@ public class Game {
 
   private static class Roll {
     private final int score;
-    private final boolean addNextRollAsBonus;
-    private final boolean addSecondNextRollAsBonus;
+    private final Bonus bonus;
 
-    public Roll(int score, boolean addNextRollAsBonus, boolean addSecondNextRollAsBonus) {
+    public Roll(int score, Bonus bonus) {
       this.score = score;
-      this.addNextRollAsBonus = addNextRollAsBonus;
-      this.addSecondNextRollAsBonus = addSecondNextRollAsBonus;
+      this.bonus = bonus;
     }
 
     public int getScore() {
       return score;
     }
 
-    public boolean shouldAddNextRollAsBonus() {
-      return addNextRollAsBonus;
+    public boolean isStrike() {
+      return Bonus.STRIKE == bonus;
     }
 
-    public boolean shouldAddSecondNextRollAsBonus() {
-      return addSecondNextRollAsBonus;
+    public boolean isSpareOrStrike() {
+      return Bonus.SPARE == bonus || Bonus.STRIKE == bonus; // Could use != Bonus.NONE, but it's ugly
     }
+  }
+
+  private enum Bonus {
+    STRIKE,
+    SPARE,
+    NONE
   }
 }
